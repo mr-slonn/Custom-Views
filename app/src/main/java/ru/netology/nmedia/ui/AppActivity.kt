a68daea59c6e4e2fcbbe90584c4055c18024eb27
192.168.0.21 <antonov_vv@spbexp.ru>
2023-08-17 12:37:26 +0300

    first commit

---
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -0,0 +1,29 @@
+package ru.netology.nmedia.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
+
+class AppActivity : AppCompatActivity(R.layout.activity_app) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        findViewById<StatsView>(R.id.stats).data = listOf(
+//            0.25F,
+//            0.25F,
+//            0.25F,
+//            0.25F,
+//        )
+
+        findViewById<StatsView>(R.id.stats).data = listOf(
+            50F,
+            100F,
+            25F,
+            500F,
+        )
+
+
+
+
+    }
+}
